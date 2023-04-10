9c6fbe243772979a0c98aefbbaed15af66d4787c
정승현 <tmdgusya@naver.com>
2023-04-10 10:25:20 +0900

    add functions - get file list in dir - create folder

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,11 @@
+import os.path
 from datetime import datetime
+from os import listdir
+from os.path import isfile, join
+
+
+def get_files(path):
+    return [f for f in listdir(path) if isfile(join(path, f))]
 
 
 def day_time_generator() -> str:
@@ -10,3 +17,11 @@ def day_time_generator() -> str:
     gen = now.strftime("%y-%m-%d")
     print(f"date time : {gen}")
     return gen
+
+
+def create_folder(dir):
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+    except OSError:
+        pass
